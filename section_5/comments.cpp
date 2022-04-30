9c925036248c356abd682802d767ed4ad9a25cc1
Vaibhav Kadam <vaibhav.kadam@getpeppermint.co>
2022-04-30 10:32:20 +0530

    added comments section

---
+++ section_5/comments.cpp
@@ -0,0 +1,30 @@
+/*
+Comments are notes annotations which are human readble
+
+Comments does not go to compiler
+
+CPP has two styles of commenting
+
+
+1. SIngle line comment
+ //This a first type
+
+2. Multi line comment has this entire block
+
+
+*/
+
+
+#include <iostream>
+
+int main()
+{
+    int number;
+
+    std::cout<<"Enter a number from 1 to 100";
+    std::cin>>number;
+
+    std::cout<<"Thats the number entered  "<<number<<std::endl;
+
+    return 0;
+}
