0e20743f34f5c619fda67454d83c377a74d9071d
Vaibhav Kadam <vaibhav.kadam@getpeppermint.co>
2022-04-30 10:31:30 +0530

    added cpp preprocessor

---
+++ section_5/cpp_processor.cpp
@@ -0,0 +1,24 @@
+/*
+CPP preprocessor is program that sources your program before it starts
+
+looks for preprocesser directive that starts with #
+
+
+#include <iostream>
+When preprocessers sees this directive it replaces with file included
+So by the time compiler sees the src code all comments are striped
+and all preprocessors are removed.
+
+
+cpp preprocessor does not understand cpp it simply follows preprocessor directive and
+gets src code ready for compiler
+
+
+#include "myfile.h"
+
+#if
+#elif
+#else
+#endif
+
+*/
